12df2998b6b7c79e5077021475c1ced171a875a2
Tol <c.vandertol@utwente.nl>
2025-11-05 23:20:46 +0100

    default run

--- run_FLOX2biophys.m
+++ run_FLOX2biophys.m
@@ -11,10 +11,9 @@ for k = 1:1%length(subdir)
     for j = 1:1%length(subsubdir)
         path_FLOX = [direc subdir(k).name '\' subsubdir(j).name '\'];
         path_specfit = path_FLOX; % I placed the SIF in the same folder as the Reflectance
-        L2valdata = FLOX2biophys(path_FLOX,path_specfit,0);
+        L2valdata = FLOX2biophys(path_FLOX,path_specfit,1);
         filename = [direc subdir(k).name '\' subsubdir(j).name '\' subdir(k).name '_' subsubdir(j).name '_retrieval.nc'];
         %writeL2C(filename,L2C_retrieval)
         save( ['output\' subdir(k).name '_' subsubdir(j).name '_L2valdata.mat'],'L2valdata')
     end
 end
-r
