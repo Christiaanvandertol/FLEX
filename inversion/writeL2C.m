d2ace7ea1a284257de71c5c32d5c1f892eb1ded5
Tol <c.vandertol@utwente.nl>
2025-12-04 14:41:48 +0100

    correct bug in CSV output writing

--- inversion/writeL2C.m
+++ inversion/writeL2C.m
@@ -5,12 +5,20 @@ fprintf(fid,'%s,%s,%s,%s,%s,%s,\r\n','time','sza','fAPAR','fAPAR_unc','fAPARchl'
 for k = 1:length(L2valdata)
     time = (L2valdata(k).results.L2biophys.t);
     for j = 1:length(L2valdata(k).results.L2biophys.fAPAR)
-        fprintf(fid,'%20s,%5.4f,%5.4f,%5.4f,%5.4f\n',time(j,:),...
-            L2valdata(k).results.L2biophys.sza(j), ...
+        %
+        x = [L2valdata(k).results.L2biophys.sza(j), ...
             L2valdata(k).results.L2biophys.fAPAR(j), ...
-            L2valdata.results.L2biophys.fAPAR_unc, ...
+            L2valdata(k).results.L2biophys.fAPAR_unc, ...
             L2valdata(k).results.L2biophys.fAPARchl(j), ...
-            L2valdata.results.L2biophys.fAPARchl_unc);
+            L2valdata(k).results.L2biophys.fAPARchl_unc];
+
+        %fprintf(fid,'%20s,%5.4f,%6.4f,%6.4f,%6.4,%6.4f\r\n',time(j,:),x);
+        fprintf(fid,'%20s',time(j,:));
+        for z = 1:length(x)
+            fprintf(fid,',%5.4f',x(z));
+        end
+         fprintf(fid,'\r\n');
+
     end
 end
 fclose(fid);
@@ -26,24 +34,14 @@ end
 fclose(fid);
 %%
 fid = fopen([filename 'bio.csv'],'w');
-if ~isfield(L2valdata(k).results.L2biophys, 'FQE')
-    fprintf(fid,'%s,%s,%s,%s,%s,%s,%s\r\n','time','LCC','LCC_unc','LCCAR','LCCAR_unc','LAR','LAI_unc');
-else
-    fprintf(fid,'%s,%s,%s,%s,%s,%s,%s,%s,%s\r\n','time','LCC','LCC_unc','LCCAR','LCCAR_unc','LAI','LAI_unc','FQE','FQE_unc' );
-end
+   fprintf(fid,'%s,%s,%s,%s,%s,%s,%s\r\n','time','LCC','LCC_unc','LCCAR','LCCAR_unc','LAR','LAI_unc');
 
 for k = 1:length(L2valdata)
 time = (L2valdata(k).results.L2biophys.time_full);
-    if ~isfield(L2valdata(k).results.L2biophys, 'FQE')
-        fprintf(fid,'%20s,%7.2f,%7.2f,%7.2f,%7.2f,%7.2f,%7.2f\n',time(j,:),L2valdata(k).results.L2biophys.LCC,L2valdata.results.L2biophys.LCCunc, ...
+
+        fprintf(fid,'%20s,%7.2f,%7.2f,%7.2f,%7.2f,%7.2f,%7.2f\n',time(1,1:11),L2valdata(k).results.L2biophys.LCC,L2valdata.results.L2biophys.LCCunc, ...
             L2valdata(k).results.L2biophys.LCAR,L2valdata.results.L2biophys.LCARunc,...
             L2valdata(k).results.L2biophys.LAI,L2valdata.results.L2biophys.LAIunc);
-    else
-        fprintf(fid,'%20s,%7.2f,%7.2f,%7.2f,%7.2f,%7.2f,%7.2f,%7.2f,%7.2f\n',time(j,:),L2valdata(k).results.L2biophys.LCC,L2valdata.results.L2biophys.LCCunc, ...
-            L2valdata(k).results.L2biophys.LCAR,L2valdata.results.L2biophys.LCARunc,...
-            L2valdata(k).results.L2biophys.LAI,L2valdata.results.L2biophys.LAIunc,...
-            L2valdata(k).results.L2biophys.FQE,L2valdata.results.L2biophys.FQE_unc);
-    end
 end
 fclose(fid);
 
@@ -76,3 +74,15 @@ for k = 1:length(L2valdata)
 end
 fclose(fid);
 
+
+if ~isfield(L2valdata(k).results.L2biophys, 'FQE')
+fid = fopen([filename 'FQE.csv'],'w');
+fprintf(fid,'%s,%s,%s\r\n','time','FQE','FQE_unc');
+for k = 1:length(L2valdata)
+    time = (L2valdata(k).results.L2biophys.time_full);
+    for j = 1:length(L2valdata(k).results.L2biophys.APARchl)
+        fprintf(fid,'%20s,%7.2f,%7.2f\n',time(j,:),L2valdata(k).results.L2biophys.FQE(j),L2valdata.results.L2biophys.FQE_unc(j));
+    end
+end
+fclose(fid);
+end
